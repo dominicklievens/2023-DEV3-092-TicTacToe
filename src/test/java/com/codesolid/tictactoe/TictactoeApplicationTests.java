44f268092ff5aa17f57b492de72587bdc370e0fb
dominicklievens <lievens_dominick1992@hotmail.com>
2023-09-13 11:22:57 +0200

    Init project

---
+++ src/test/java/com/codesolid/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesolid.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
